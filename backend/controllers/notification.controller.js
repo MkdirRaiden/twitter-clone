ccc4d0d4853f2260fae42c93299e1ce17c2ad3d1
Muktadir <1996raiden27@gmail.com>
2024-06-10 20:14:13 +0530

    notification-controller added

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,28 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const notification = await Notification.find({ to: userId }).populate({
+      path: "from",
+      select: "userName profileImg",
+    });
+    await Notification.updateMany({ to: userId }, { read: true });
+    res.status(200).json(notification);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error!" });
+    console.log("Error in getNotifications controller: ", error);
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    await Notification.deleteMany({ to: userId });
+    res.status(200).json({ message: "Notification deleted successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error!" });
+    console.log("Error in deleteNotification controller: ", error);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 dotenv.config(); //to read dotenv files
 cloudinary.config({
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
