f663a797f2514d3573dfcc3ea4e0394b04cefa54
Ahmed <1996raiden27@gmail.com>
2025-05-30 19:49:45 +0530

    login css added

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -33,13 +33,13 @@ const LoginPage = () => {
 
   return (
     <div className="max-w-(--breakpoint-xl) mx-auto flex h-screen md:px-10">
-      <div className="flex-1 flex justify-center items-center gap-10">
+      <div className="flex-1 flex justify-center items-center gap-10 ">
         <div className=" hidden max-w-1/2 lg:flex items-center justify-center">
           <XSvg className="w-52 h-52 fill-white" />
         </div>
-        <div>
+        <div className="max-sm:w-full max-sm:px-8">
           <form
-            className="flex gap-4 flex-col md-w-2/5"
+            className="flex gap-4 flex-col md-w-2/5 w-full"
             onSubmit={handleSubmit}
           >
             <XSvg className="w-24 lg:hidden fill-white" />
@@ -88,7 +88,7 @@ const LoginPage = () => {
               )}
             </button>
           </form>
-          <div className="flex flex-col md:items-center gap-2 mt-4">
+          <div className="flex max-sm:text-center flex-col md:items-center gap-2 mt-4">
             <p className="text-white text-lg">{"Don't"} have an account?</p>
             <Link className="md:w-2/3" to="/signup">
               <button className="btn rounded-full btn-primary text-white btn-outline w-full">
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -119,8 +119,10 @@ const SignUpPage = () => {
                 )}
               </button>
             </form>
-            <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
-              <p className="text-white text-lg">Already have an account?</p>
+            <div className="flex flex-col lg:w-2/3 gap-2 mt-4 mx-auto w-full">
+              <p className="text-white text-lg max-sm:text-center">
+                Already have an account?
+              </p>
               <Link to="/login">
                 <button className="btn rounded-full btn-primary text-white btn-outline w-full">
                   Sign in <MdOutlineArrowForward />
